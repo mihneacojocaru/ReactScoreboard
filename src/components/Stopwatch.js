b44ac2407cac36efe1c8917c623fc62f1b4fca80
mihneacojocaru <mihnea.co@gmail.com>
2022-04-06 19:15:23 +0200

    stopwatch

--- src/components/Stopwatch.js
+++ src/components/Stopwatch.js
@@ -1,9 +1,36 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
 
 const Stopwatch = () => {
-  return (
-    <div className='app-header__stopwatch'>Stopwatch</div>
-  )
-}
+  const [time, setTime] = useState(0);
+  const [timeOn, setTimeOn] = useState(false);
 
-export default Stopwatch
+  useEffect(() => {
+    let interval = null;
+
+    if (timeOn) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [timeOn]);
+
+  return (
+    <div className="app-header__stopwatch">
+      <p>Stopwatch</p>
+      <p className="app-header__stopwatch-display">{time}</p>
+      <div className="app-header__stopwatch-buttons">
+        {!timeOn ? (
+          <button onClick={() => setTimeOn(true)}>Start</button>
+        ) : (
+          <button onClick={() => setTimeOn(false)}>Stop</button>
+        )}
+        <button onClick={() => setTime(0)}>Reset</button>
+      </div>
+    </div>
+  );
+};
+
+export default Stopwatch;
